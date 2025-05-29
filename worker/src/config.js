ca01deab241f7bd49ec54f2838267b79d2c3ea4f
build-africa-eng <afrcanfuture@gmail.com>
2025-05-29 14:30:34 +0300

    Update config.js

--- worker/src/config.js
+++ worker/src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret',
+  jwtSecret: process.env.JWT_SECRET || '123bc901ad1260cc***',
   d1DatabaseId: 'DB-f6100f31-4a5d-4005-ba82-2b2e22eee355',
   kvNamespaceId: 'b619d313838340b5921e0bf8e492799f-kv',
 };
