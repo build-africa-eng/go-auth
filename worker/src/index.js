347638b0bc6ee403a639e0c810dda45b819f1c3c
build-africa-eng <afrcanfuture@gmail.com>
2025-05-29 13:28:55 +0300

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,4 +1,4 @@
-import { Router } from 'itty-router';
+limport { Router } from '@cloudflare/itty-router';
 import { config } from './config.js';
 import { UserService } from './users.js';
 import { TokenService } from './tokens.js';
@@ -9,6 +9,25 @@ import { login } from './handlers/login.js';
 import { refresh } from './handlers/refresh.js';
 
 const router = Router();
+
+// Add CORS headers to all responses
+router.all('*', (request, response) => {
+  response.headers.set('Access-Control-Allow-Origin', '[invalid url, do not cite]);
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  return response;
+});
+
+// Handle preflight OPTIONS requests
+router.options('*', () => {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '[invalid url, do not cite],
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
+});
 
 router.post('/register', async (request, env) => {
   const userService = new UserService(env.DB);
