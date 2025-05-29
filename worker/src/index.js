01a07edb5060606020043527a96954c7313467dd
build-africa-eng <afrcanfuture@gmail.com>
2025-05-29 13:32:07 +0300

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,4 +1,4 @@
-import { Router } from '@cloudflare/itty-router';
+import { Router } from 'itty-router';
 import { config } from './config.js';
 import { UserService } from './users.js';
 import { TokenService } from './tokens.js';
